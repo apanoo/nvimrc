4769fed2e795d287844f54473fd7339f4f5ee853
apanoo <apanoo@126.com>
2022-07-28 13:32:29 +0800

    feat(autocmd): 'set expandtab' for javascript & typescript & ...

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -2,11 +2,12 @@ local augroup = vim.api.nvim_create_augroup   -- Create/get autocommand group
 local autocmd = vim.api.nvim_create_autocmd   -- Create autocommand
 
 -- Set indentation to 2 spaces
+-- 'set expandtab'表示空格代替tab
 augroup('setIndent', { clear = true })
 autocmd('Filetype', {
   group = 'setIndent',
   pattern = { 'xml', 'html', 'xhtml', 'css', 'scss', 'javascript', 'typescript',
     'yaml', 'lua', 'typescriptreact', 'javascriptreact'
   },
-  command = 'setlocal shiftwidth=2 tabstop=2'
+  command = 'setlocal shiftwidth=2 tabstop=2 expandtab'
 })
