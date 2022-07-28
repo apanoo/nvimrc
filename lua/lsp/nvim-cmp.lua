17f7503a3a5d714c6374e3ebe9e1afff01e399a0
apanoo <apanoo@126.com>
2022-07-28 16:44:12 +0800

    fix(cmp): remove redefinition of cmp

--- lua/lsp/nvim-cmp.lua
+++ lua/lsp/nvim-cmp.lua
@@ -63,8 +63,4 @@ cmp.setup.cmdline(":", {
 -- 可选： 补全function结束后，自动补全括号
 -- 与lspconfig中的usePlaceholders不冲突
 local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-local cmp_status_ok, cmp = pcall(require, "cmp")
-if not cmp_status_ok then
-	return
-end
 cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done({ map_char = { tex = "" } }))
